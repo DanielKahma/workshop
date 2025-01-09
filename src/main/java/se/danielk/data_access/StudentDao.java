21df803b6f2868e568b4405fad6ee06b952f52f6
Daniel.Kahma <you@example.com>
2025-01-09 11:21:43 +0100

    part 1 done with implements

--- src/main/java/se/danielk/data_access/StudentDao.java
+++ src/main/java/se/danielk/data_access/StudentDao.java
@@ -1,4 +1,13 @@
 package se.danielk.data_access;
 
+import se.danielk.models.Student;
+
+import java.util.List;
+
 public interface StudentDao {
+
+    Student save (Student student);
+    Student find (int id);
+    List <Student> findAll();
+    void delete (int id);
 }
--- src/main/java/se/danielk/data_access/StudentDaoListImpl.java
+++ src/main/java/se/danielk/data_access/StudentDaoListImpl.java
@@ -1,7 +1,32 @@
 package se.danielk.data_access;
 
 import org.springframework.stereotype.Component;
+import se.danielk.models.Student;
+
+import java.util.List;
 
 @Component
 public class StudentDaoListImpl implements StudentDao{
+
+    List<Student> students;
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
